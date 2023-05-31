13d10e205cf88f3c93ccbad3964609c5fb3a6dc2
Reeveli <panu.pessi@gmail.com>
2023-05-31 22:05:44 +0300

    Added teleportation system settings

--- scripts/SPG/CBApreinit.hpp
+++ scripts/SPG/CBApreinit.hpp
@@ -1,6 +1,10 @@
 /*
-.HPP file to keep Description.ext as clean as possible.
-Theese are all CBA Pre Init classes.
+ * Author: Reeveli & Killet
+ * Part of SPG default scripts.
+ * Includes all custom CBA extended preinit calls to keepd escription.ext clean.
+
+1.1
+	Added Reeveli's teleportation system settings
 */
 
 class KLT_CBA_Settings_keybinds {init = "call compile preProcessFileLineNumbers 'scripts\KLT\Zeus\zeuscam\CBAKeys.sqf'";};              //Killets Zeus Tools
@@ -9,6 +13,7 @@ class SPG_settings {init = "call compile preprocessFileLineNumbers 'scripts\SPG\
 class Reeveli_Advanced_gestures {init = " call compile preprocessFileLineNumbers 'scripts\Reeveli_fnc\Rev_gestures\Rev_advanced_gestures_settings.sqf'";};
 class Reveli_user_markers {init = " call compile preprocessFileLineNumbers 'scripts\Reeveli_fnc\Rev_markers_script\Rev_marker_settings.sqf'";};
 class Reeveli_Rectricted_Zeus {init = " call compile preprocessFileLineNumbers 'scripts\Reeveli_fnc\Rev_restricted_zeus\Rev_RZ_settings.sqf'";};
+class Rev_tp_settings {init = "call compile preprocessFileLineNumbers 'scripts\Reeveli_fnc\Rev_teleport\Rev_tp_settings.sqf'";};
 
 
 
